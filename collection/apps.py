4a44e4cd0a1161992a0353e9361bb8289c7ee76b
Enrique <me@enriquedenobrega.com>
2022-09-26 00:57:10 +0100

    Update sets data on server start

--- collection/apps.py
+++ collection/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CollectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'collection'
+
+    def ready(self):
+        from .models import mtg
+        mtg.Set.Update()
