a22f635c9c45d6a1533cb23aa8edb4d8987870b9
Enrique <me@enriquedenobrega.com>
2022-09-26 00:57:33 +0100

    Link urls for mtg

--- collection/urls.py
+++ collection/urls.py
@@ -3,4 +3,6 @@ import collection.views as cv
 
 urlpatterns = [
     path('', cv.index.as_view(), name='collection_index'),
+    path('mtg/', cv.mtg_index.as_view(), name='mtg_index'),
+    path('mtg/sets', cv.mtg_set_list.as_view(), name='mtg_set_list'),
 ]
