89265ec910e71ed1c6129b403806357a42b759de
Enrique <me@enriquedenobrega.com>
2022-09-26 00:59:44 +0100

    Display all Set's using Tabulator

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,47 @@
+import json
 from django.shortcuts import render
 from django.views import View
 from collection.models import mtg
 
-# Create your views here.
+
 class index(View):
     def get(self, request):
-        mtg.Set.Update()
+        return render(
+            request,
+            'index.html',
+            {
+
+            }
+        )
+
+
+class mtg_index(View):
+    def get(self, request):
+        return render(
+            request,
+            'mtg/index.html',
+            {
+                
+            }
+        )
+
+
+class mtg_set_list(View):
+    def get(self, request):
+        data = list(mtg.Set.objects.all().values())
+
+        return render(
+            request,
+            'mtg/sets.html',
+            {
+                'data': data
+            }
+        )
+
+
+class mtg_view(View):
+    def get(self, request, set_code):
+        pass
+
+    def post(self, request, set_code):
+        pass
