c1ff94988a9353ca98f6c945dd20daf7f6ecc429
Enrique <me@enriquedenobrega.com>
2022-09-27 01:18:08 +0100

    Implement code for updating mtg cards db, better todo from dump file

--- collection/models/mtg.py
+++ collection/models/mtg.py
@@ -1,4 +1,7 @@
+from email.mime import image
+import json
 from operator import truediv
+from tabnanny import check
 from django.db import models
 import requests
 
@@ -38,19 +41,105 @@ class Type(models.Model):
 # B = Black
 # R = Red
 # G = Green
+# TODO: Add converted manacost
 class Card(models.Model):
     name = models.CharField(max_length=200)
     card_set = models.ForeignKey(Set, on_delete=models.CASCADE, blank=False, null=False)
     mana_cost = models.CharField(max_length=20)
-    text = models.CharField(max_length=500)
-    flavor = models.CharField(max_length=500)
+    text = models.CharField(max_length=500, null=True)
+    flavor = models.CharField(max_length=500, null=True)
     artist = models.CharField(max_length=100)
-    collector_number = models.IntegerField(null=False)
-    power = models.CharField(max_length=3)
-    toughness = models.CharField(max_length=3)
+    collector_number = models.CharField(max_length=6, null=False)
+    power = models.CharField(max_length=3, null=True)
+    toughness = models.CharField(max_length=3, null=True)
+    scryfall_id = models.CharField(max_length=36)
+    converted_cost = models.IntegerField(null=True)
+    # image will be face, flipped will be back
+    image = models.URLField(null=True)
+    image_flipped = models.URLField(null=True)
 
-    def update_cards():
-        pass
+    def Update():
+        # Below is for updating cards via file dump
+        # f = 'UNIQUE ARTWORK DUMP FROM SCRYFALL HERE' 
+        # fi = open(f+'.json', encoding='utf8') 
+        # j = json.load(fi)
+        # print(len(Card.objects.filter(image=None)))
+        # for c in Card.objects.filter(image=None):
+        #     try:
+        #         obj = next(x for x in j if x['id'] == c.scryfall_id)
+        #     except:
+        #         continue
+        #     if 'image_uris' in obj:
+        #         c.image = obj['image_uris']['normal']
+        #     elif 'card_faces' in obj:
+        #         c.image = obj['card_faces'][0]['image_uris']['normal']
+        #         c.image_flipped = obj['card_faces'][1]['image_uris']['normal']
+        #     else:
+        #         continue
+        #     c.save()
+        # print(len(Card.objects.filter(image=None)))
+        # return
+        import time     
+
+        sets = Set.objects.all()
+
+        for _set in sets:
+            r = requests.get(_set.search_uri)
+            _json = r.json()
+            current_count = len(Card.objects.filter(card_set=_set))
+            cc = _json['card_count']
+            if current_count != cc:
+                print(f'{_set.name} is missing {cc-current_count} card/s')
+                print('Adding cards')
+                s_uri = _json['search_uri']
+                print(f'Making request to {s_uri}')
+                r = requests.get(_json['search_uri'])
+                print(r.status_code)
+                if r.status_code != 200:
+                    continue           
+                while True:
+                    cl_json = r.json()
+
+                    for d in cl_json['data']:
+                        if Card.objects.filter(scryfall_id=d['id']).exists():
+                            temp_name = d['name']
+                            temp_collectors = d['collector_number']
+                            print(f'Card {temp_name}, {temp_collectors}, of set {_set.shorthand} exists already')
+                            continue
+
+                        new_card = Card()
+
+                        new_card.name = d['name']
+                        new_card.card_set = _set
+                        if 'mana_cost' in d:
+                            new_card.mana_cost = d['mana_cost']
+                        if 'oracle_text' in d:
+                            new_card.text = d['oracle_text']
+                        if 'flavor_text' in d:
+                            new_card.flavor = d['flavor_text']
+                        new_card.artist = d['artist']
+                        new_card.collector_number = d['collector_number']
+                        if 'power' in d:
+                            new_card.power = d['power']
+                        if 'toughness' in d:
+                            new_card.toughness = d['toughness']
+                        new_card.scryfall_id = d['id']
+
+                        if 'image_uris' in _json:
+                            c.image = _json['image_uris']['normal']
+                        elif 'card_faces' in _json:
+                            c.image = _json['card_faces'][0]['image_uris']['normal']
+                            c.image_flipped = _json['card_faces'][1]['image_uris']['normal']
+                        else:
+                            print('Error: No images found for card')
+
+                        new_card.save()
+
+                    if not cl_json['has_more']:
+                        print(f'Finished Updating {_set.name}')
+                        break
+                    time.sleep(0.150)
+                    r = requests.get(cl_json['next_page'])
 
 
 class TypeLine(models.Model):
