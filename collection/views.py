8a9799461f47aff537ad4d15967dde52d979fd1e
Enrique <me@enriquedenobrega.com>
2022-09-29 22:18:34 +0100

    Add menus for navigation around MTG

--- collection/views.py
+++ collection/views.py
@@ -28,8 +28,8 @@ class mtg_index(View):
 
 class mtg_set_list(View):
     def get(self, request):
-        data = list(mtg.Set.objects.all().values())
-
+        _data = list(mtg.Set.objects.all().values())
+        data = [{'name':x['name'], 'shorthand':x['shorthand'], 'set_type':mtg.SetType.objects.filter(id=x['set_type_id']).first().name} for x in _data]
         return render(
             request,
             'mtg/sets.html',
@@ -40,15 +40,36 @@ class mtg_set_list(View):
 
 
 class mtg_view_set(View):
-    def get(self, request):
-        data = mtg.Card.objects.filter(card_set__shorthand='m19')
+    def get(self, request, set_short):
+        set_name = mtg.Set.objects.filter(shorthand=set_short).first().name
+
+        _data = list(mtg.Card.objects.filter(card_set__shorthand=set_short).values())
+        data = []
+
+        for d in _data:
+            for u in d:
+                if u == 'rarity_id':
+                    d[u] = mtg.Rarity.objects.filter(id=d[u]).first().name.capitalize()
+                if d[u] == None:
+                    # None cannot be used in tabulator
+                    d[u] = ''
+            _temp = d
+
+            type_line_str = ''
+            type_line = mtg.TypeLine.objects.filter(card__id=d['id'])
+            for tl in type_line:
+                type_line_str = type_line_str + tl.type.name.lower().capitalize() + ' '
+            _temp['type_line'] = type_line_str
+            data.append(_temp)
+
         return render(
             request,
             'mtg/view_set.html',
             {
+                'card_set':set_name,
                 'data': data
             }
         )
 
-    def post(self, request):
+    def post(self, request, set_short):
         pass
