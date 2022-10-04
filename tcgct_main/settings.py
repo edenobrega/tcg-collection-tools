e05bd3280ddfda17f465bddac6d36d5e42963582
Enrique <me@enriquedenobrega.com>
2022-10-04 22:40:35 +0100

    Make changes for MSSQL Server use

--- tcgct_main/settings.py
+++ tcgct_main/settings.py
@@ -84,13 +84,20 @@ WSGI_APPLICATION = 'tcgct_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'tcgct',
+        'HOST': 'DESKTOP-80083JA\SQLEXPRESS'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
