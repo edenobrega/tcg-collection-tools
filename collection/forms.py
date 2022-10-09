7b883419ccdcb63d874bbc0ac064acee4876959c
Enrique <me@enriquedenobrega.com>
2022-10-09 21:25:54 +0100

    Remove card_set value from forms.py

--- collection/forms.py
+++ collection/forms.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from pickle import FALSE
 from django import forms
 from pkg_resources import require
@@ -6,8 +7,8 @@ from collection.models.mtg import Set, CardType
 class MTGSearchForm(forms.Form):
     name = forms.CharField(max_length=200, required=False)
     converted_cost = forms.CharField(max_length=6, required=False)
-    card_set = forms.ModelChoiceField(queryset=Set.objects.all(), required=False)
     types_select = forms.ModelChoiceField(queryset=CardType.objects.all(), required=False)
     types = forms.CharField(max_length=500, required=False)
     power = forms.CharField(max_length=3, required=False)
     toughness = forms.CharField(max_length=3, required=False)
+    search_collection = forms.BooleanField(initial=False, required=False)
